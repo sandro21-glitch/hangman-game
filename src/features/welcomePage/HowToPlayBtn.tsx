2aa8797fa31454ee854f3fd47af351aaae10afaf
Sandro <vipsandro11@gmail.com>
2024-03-07 21:01:11 +0400

    initial commit

---
+++ src/features/welcomePage/HowToPlayBtn.tsx
@@ -0,0 +1,12 @@
+const HowToPlayBtn = () => {
+  return (
+    <button
+      className="uppercase py-[.8rem] px-[4rem] rounded-full text-[2rem] tracking-[.1rem]
+   cursor-pointer bg-primary-blue hover:bg-opacity-80 transition-all ease-in duration-150 inset--3 text-white "
+    >
+      how to play
+    </button>
+  );
+};
+
+export default HowToPlayBtn;
