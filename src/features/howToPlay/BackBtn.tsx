6266652f4f9c55f3c0059a49ad67683e86878dc9
Sandro <vipsandro11@gmail.com>
2024-03-08 18:06:21 +0400

    feat: create BackBtn.tsx

---
+++ src/features/howToPlay/BackBtn.tsx
@@ -0,0 +1,23 @@
+import { useNavigate } from "react-router-dom";
+import backLogo from "/assets/back-button.svg";
+import useSound from "../../hooks/useSound";
+const BackBtn = () => {
+  const navigate = useNavigate();
+  const { playSound } = useSound();
+  const handleBack = () => {
+    playSound();
+    navigate("/");
+  };
+  return (
+    <button
+      onClick={handleBack}
+      className="w-[3.5rem] h-[3.5rem] md:w-[6rem] md:h-[5.5rem] flex justify-center items-center rounded-full  button-gradient
+    cursor-pointer outline-none border-none
+  "
+    >
+      <img src={backLogo} alt="back icon" className="bg-none w-[1.5rem] h-[1.5rem] md:w-auto md:h-auto" />
+    </button>
+  );
+};
+
+export default BackBtn;
