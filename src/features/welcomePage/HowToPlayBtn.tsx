8bb2bbc368d6e849753a03bc1576db6deb5b2606
Sandro <vipsandro11@gmail.com>
2024-03-08 15:52:03 +0400

    update HowToPlayBtn.tsx and PlayBtn.tsx

--- src/features/welcomePage/HowToPlayBtn.tsx
+++ src/features/welcomePage/HowToPlayBtn.tsx
@@ -1,6 +1,13 @@
+import useSound from "../../hooks/useSound";
+
 const HowToPlayBtn = () => {
+  const { playSound } = useSound();
+  const handleClickButton = () => {
+    playSound();
+  };
   return (
     <button
+      onClick={handleClickButton}
       className="uppercase py-[.8rem] px-[4rem] rounded-full text-[2rem] tracking-[.1rem]
    cursor-pointer bg-primary-blue hover:bg-opacity-80 transition-all ease-in duration-150 inset--3 text-white "
     >
--- src/features/welcomePage/PlayBtn.tsx
+++ src/features/welcomePage/PlayBtn.tsx
@@ -1,11 +1,17 @@
 import { useNavigate } from "react-router-dom";
 import playIcon from "/assets/play-button.svg";
+import useSound from "../../hooks/useSound";
 
 const PlayBtn = () => {
   const navigate = useNavigate();
+  const { playSound } = useSound();
+  const handleClickPlay = () => {
+    playSound();
+    navigate("/game");
+  };
   return (
     <button
-      onClick={() => navigate("/game")}
+      onClick={handleClickPlay}
       className="w-[12rem] h-[12rem] flex justify-center items-center rounded-full inset--2 button-gradient
     cursor-pointer outline-none border-none
   "
