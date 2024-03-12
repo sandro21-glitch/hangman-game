c308d3262f011ef11349bd4c6577671cf2179480
Sandro <vipsandro11@gmail.com>
2024-03-12 21:12:47 +0400

    update hangmanSlice.ts

--- src/features/game/hangmanSlice.ts
+++ src/features/game/hangmanSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-// import type { PayloadAction } from '@reduxjs/toolkit'
+import type { PayloadAction } from "@reduxjs/toolkit";
 import data from "../../data.json";
 
 export interface HangmanState {
@@ -10,18 +10,38 @@ export interface HangmanState {
     }[];
   };
   categoryNames: string[];
+  activeCategory: { name: string; selected: boolean }[];
+  usedChars: string[];
 }
 
 const initialState: HangmanState = {
   categories: data.categories,
   categoryNames: Object.keys(data.categories),
+  activeCategory: [],
+  usedChars: [],
 };
 export const counterSlice = createSlice({
   name: "hangman",
   initialState,
-  reducers: {},
+  reducers: {
+    setActiveCategoryList: (state, action: PayloadAction<string>) => {
+      const categoryName = action.payload;
+      const { categories } = state;
+
+      // check if categoryName is a key in categories
+      if (categories.hasOwnProperty(categoryName)) {
+        state.activeCategory = categories[categoryName].map((item) => ({
+          name: item.name,
+          selected: item.selected,
+        }));
+      }
+    },
+    setClickedChar: (state, action: PayloadAction<string>) => {
+      state.usedChars.push(action.payload);
+    },
+  },
 });
 
-export const {} = counterSlice.actions;
+export const { setActiveCategoryList, setClickedChar } = counterSlice.actions;
 
 export default counterSlice.reducer;
