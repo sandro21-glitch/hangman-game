3ed39234868d095181d1d7cba0267e495fde60ef
Sandro <vipsandro11@gmail.com>
2024-03-12 21:12:37 +0400

    update Game.tsx

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,16 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PickCategory from "../features/game/pickCategory/PickCategory";
 import GamePage from "../features/game/gamePage/GamePage";
+import { useAppSelector } from "../hooks";
 
 const Game = () => {
   const [gameBegin, setGameBegin] = useState({ start: false, category: "" });
+  const [categoryIndex, setCategoryIndex] = useState<number>(0);
+  const { activeCategory } = useAppSelector((store) => store.game);
+  useEffect(() => {
+    if (activeCategory && activeCategory.length > 0) {
+      const randomIndex = Math.floor(Math.random() * activeCategory.length);
+      setCategoryIndex(randomIndex);
+    }
+  }, [activeCategory]);
 
   return (
     <section>
       {!gameBegin.start ? (
         <PickCategory setGameBegin={setGameBegin} />
       ) : (
-        <GamePage />
+        <GamePage categoryIndex={categoryIndex} />
       )}
     </section>
   );
