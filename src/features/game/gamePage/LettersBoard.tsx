2b8af3d3f49486d158b84d9e3aec6af7d2454cde
Sandro <vipsandro11@gmail.com>
2024-03-12 21:12:55 +0400

    update LettersBoard.tsx

--- src/features/game/gamePage/LettersBoard.tsx
+++ src/features/game/gamePage/LettersBoard.tsx
@@ -1,14 +1,21 @@
+import { useAppDispatch } from "../../../hooks";
+import { setClickedChar } from "../hangmanSlice";
+
 const LettersBoard = () => {
   const alphabetArray = Array.from({ length: 26 }, (_, index) =>
     String.fromCharCode("A".charCodeAt(0) + index)
   );
-
+  const dispatch = useAppDispatch();
+  const handleAddChar = (char: string) => {
+    dispatch(setClickedChar(char));
+  };
   return (
     <div className="flex gap-5 flex-wrap justify-center items-center">
       {alphabetArray.map((a, index) => {
         return (
           <button
             type="button"
+            onClick={() => handleAddChar(a)}
             key={index}
             className="w-[7rem] h-[5rem] bg-white rounded-[1rem]
              flex justify-center items-center text-[#261676]
