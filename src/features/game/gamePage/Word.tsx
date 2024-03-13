7d8082ac351cc23e903e70c88f809be157d5e9c0
Sandro <vipsandro11@gmail.com>
2024-03-13 17:56:43 +0400

    refactor: change Word Component to display each word on a new line with gaps between words

--- src/features/game/gamePage/Word.tsx
+++ src/features/game/gamePage/Word.tsx
@@ -9,28 +9,39 @@ const Word = ({ categoryIndex }: WordTypes) => {
   const word = activeCategory[categoryIndex].name;
 
   const chars = usedChars.map((char) => char.toLowerCase());
+  console.log(word);
 
   const renderWord = () => {
-    return word.split("").map((char, index) => {
-      const isCharUsed = chars.includes(char.toLowerCase());
-      return char === " " ? (
-        <span key={index} className="mx-[3.5rem]"></span>
-      ) : (
-        <span
-          key={index}
-          className={`bg-primary-blue max-w-[7rem] max-h-[6rem] m-1 p-[3.5rem]
-             rounded-[2rem] inset--3 inline-flex items-center justify-center text-[4rem] ${
-               isCharUsed ? "text-white" : ""
-             }`}
-        >
-          {isCharUsed ? char : ""}
-        </span>
-      );
-    });
+    const words = word.split(" ");
+
+    return (
+      <div className="mt-[3rem] mb-[5rem] flex justify-center flex-wrap items-center">
+        {words.map((wordPart, index) => (
+          <div key={index} className="flex mr-[7rem]">
+            {wordPart.split("").map((char, charIndex) => {
+              const isCharUsed = chars.includes(char.toLowerCase());
+              return char === " " ? (
+                <span key={charIndex}></span>
+              ) : (
+                <span
+                  key={charIndex}
+                  className={`bg-primary-blue max-w-[7rem] max-h-[6rem] m-1 p-[3.5rem]
+                    rounded-[2rem] inset--3 inline-flex items-center justify-center text-[4rem] ${
+                      isCharUsed ? "text-white" : ""
+                    }`}
+                >
+                  {isCharUsed ? char : ""}
+                </span>
+              );
+            })}
+          </div>
+        ))}
+      </div>
+    );
   };
 
   return (
-    <div className=" mt-[3rem] mb-[5rem] flex justify-center items-center">
+    <div className=" mt-[3rem] mb-[5rem] flex justify-center flex-wrap items-center">
       {renderWord()}
     </div>
   );
