d392a6b3ab8217f82645e7d121f40cf00df035af
Sandro <vipsandro11@gmail.com>
2024-03-13 22:04:04 +0400

    refactor: Update MenuList component to handle setIsMenuOpen and setGameBegin props

--- src/features/game/pauseMenu/MenuList.tsx
+++ src/features/game/pauseMenu/MenuList.tsx
@@ -1,14 +1,32 @@
-const MenuList = () => {
+import { useNavigate } from "react-router-dom";
+
+type MenuListTypes = {
+  setIsMenuOpen: (isOpen: boolean) => void;
+  setGameBegin: (value: { start: boolean; category: string }) => void;
+};
+
+const MenuList = ({ setIsMenuOpen, setGameBegin }: MenuListTypes) => {
+  const navigate = useNavigate();
+  const handleQuitGame = () => {
+    navigate("/");
+  };
+  const handleResetGame = () => {
+    setGameBegin({ start: false, category: "" });
+    setIsMenuOpen(false);
+  };
+
   return (
     <ul className="flex flex-col gap-7">
       <li
         className="bg-primary-blue rounded-[40px] cursor-pointer px-[4rem] py-[.8rem]
        inset--3 border-none text-[2rem] text-white uppercase tracking-wider text-center
         hover:bg-blue-500 transition-all ease-in duration-150"
+        onClick={() => setIsMenuOpen(false)}
       >
         Continue
       </li>
       <li
+        onClick={() => handleResetGame()}
         className="bg-primary-blue rounded-[40px] cursor-pointer px-[4rem] py-[.8rem]
        inset--3 border-none text-[2rem] text-white uppercase tracking-wider text-center
         hover:bg-blue-500 transition-all ease-in duration-150"
@@ -16,6 +34,7 @@ const MenuList = () => {
         New Category
       </li>
       <li
+        onClick={handleQuitGame}
         className=" rounded-[40px] cursor-pointer px-[4rem] py-[.8rem]
         quit-game border-none text-[2rem] text-white uppercase tracking-wider text-center hover:opacity-85
           transition-opacity ease-in duration-150"
