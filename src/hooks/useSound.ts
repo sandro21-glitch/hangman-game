cd76d9e7b38f616b6ff3db369beeb09f8b350c40
Sandro <vipsandro11@gmail.com>
2024-03-13 18:50:39 +0400

    feat: change audio volume

--- src/hooks/useSound.ts
+++ src/hooks/useSound.ts
@@ -1,10 +1,11 @@
 import { useState, useRef, useEffect } from "react";
 import soundUrl from "/assets/sounds/clickBubble.wav";
-const useSound = () => {
+const useSound = (volume = 0.1) => {
   const audioRef = useRef(new Audio(soundUrl));
   const [isPlaying, setIsPlaying] = useState(false);
 
   const playSound = () => {
+    audioRef.current.volume = volume;
     audioRef.current.play();
     setIsPlaying(true);
   };
