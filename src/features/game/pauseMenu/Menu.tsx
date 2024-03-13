070a84ce7b4858388b2d0575bd6cf6b436eb29a6
Sandro <vipsandro11@gmail.com>
2024-03-13 22:03:52 +0400

    refactor: Pass setIsMenuOpen and setGameBegin props to MenuList in Menu component

--- src/features/game/pauseMenu/Menu.tsx
+++ src/features/game/pauseMenu/Menu.tsx
@@ -2,15 +2,20 @@ import SectionTitle from "../../../ui/SectionTitle";
 import PageOverlay from "../overlay/PageOverlay";
 import MenuList from "./MenuList";
 
-const Menu = () => {
+type MenuTypes = {
+  setIsMenuOpen: (isOpen: boolean) => void;
+  setGameBegin: (value: { start: boolean; category: string }) => void;
+};
+
+const Menu = ({ setIsMenuOpen, setGameBegin }: MenuTypes) => {
   return (
-    <div className="absolute inset-0 h-screen w-full z-[1] flex justify-center items-center">
+    <div className="absolute inset-0 h-screen w-full z-[1] flex justify-center items-center select-none">
       <PageOverlay />
       <div className="w-[37rem] h-[27rem] gradient-bg inset--1 rounded-[72px] relative flex justify-center items-center">
         <header className="absolute top-[-20%] left-0 right-0 mx-auto z-[1]">
           <SectionTitle text="Paused" />
         </header>
-        <MenuList />
+        <MenuList setIsMenuOpen={setIsMenuOpen} setGameBegin={setGameBegin} />
       </div>
     </div>
   );
