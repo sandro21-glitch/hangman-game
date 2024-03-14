3f39d9fc16b26cccbfc2f43e5f85528a4a7d726a
Sandro <vipsandro11@gmail.com>
2024-03-14 16:58:02 +0400

    feat: Add sound effects functionality

--- src/features/game/gamePage/GameHeader.tsx
+++ src/features/game/gamePage/GameHeader.tsx
@@ -1,18 +1,24 @@
 import menuLogo from "/assets/menu.svg";
 import heart from "/assets/heart.svg";
+import useSound from "../../../hooks/useSound";
 
 type GameHeaderTypes = {
   setIsMenuOpen: (isOpen: boolean) => void;
 };
 
 const GameHeader = ({ setIsMenuOpen }: GameHeaderTypes) => {
+  const { playSound } = useSound();
+  const handleOpenMenu = () => {
+    setIsMenuOpen(true);
+    playSound();
+  };
   return (
     <header className="py-10">
       <nav className="flex items-center justify-between">
         <div className="flex items-center gap-10">
           <button
             type="button"
-            onClick={() => setIsMenuOpen(true)}
+            onClick={handleOpenMenu}
             className="button-gradient h-[5.5rem] w-[5.5rem] inset--4 rounded-full flex justify-center items-center"
           >
             <img src={menuLogo} alt="menu" />
