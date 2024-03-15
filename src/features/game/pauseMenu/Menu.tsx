5c15bf19c77aaf41685758da3c4987b69cd8d58f
Sandro <vipsandro11@gmail.com>
2024-03-15 23:36:32 +0400

    feat: Extend Menu component to accept setCategoryIndex prop

--- src/features/game/pauseMenu/Menu.tsx
+++ src/features/game/pauseMenu/Menu.tsx
@@ -6,9 +6,15 @@ type MenuTypes = {
   title: string;
   setIsMenuOpen: (isOpen: boolean) => void;
   setGameBegin: (value: { start: boolean; category: string }) => void;
+  setCategoryIndex: (index: number) => void;
 };
 
-const Menu = ({ title, setIsMenuOpen, setGameBegin }: MenuTypes) => {
+const Menu = ({
+  title,
+  setIsMenuOpen,
+  setGameBegin,
+  setCategoryIndex,
+}: MenuTypes) => {
   return (
     <div className="absolute inset-0 h-screen w-full z-[1] flex justify-center items-center select-none">
       <PageOverlay />
@@ -16,7 +22,11 @@ const Menu = ({ title, setIsMenuOpen, setGameBegin }: MenuTypes) => {
         <header className="absolute top-[-20%] left-0 right-0 mx-auto z-[1]">
           <SectionTitle text={title} />
         </header>
-        <MenuList setIsMenuOpen={setIsMenuOpen} setGameBegin={setGameBegin} />
+        <MenuList
+          setIsMenuOpen={setIsMenuOpen}
+          setGameBegin={setGameBegin}
+          setCategoryIndex={setCategoryIndex}
+        />
       </div>
     </div>
   );
