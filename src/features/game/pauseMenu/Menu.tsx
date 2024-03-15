d22c82cff95026a21ac7f5ab999646540dfce951
Sandro <vipsandro11@gmail.com>
2024-03-15 21:56:10 +0400

    fix: Update Menu component to accept dynamic title prop

--- src/features/game/pauseMenu/Menu.tsx
+++ src/features/game/pauseMenu/Menu.tsx
@@ -3,17 +3,18 @@ import PageOverlay from "../overlay/PageOverlay";
 import MenuList from "./MenuList";
 
 type MenuTypes = {
+  title: string;
   setIsMenuOpen: (isOpen: boolean) => void;
   setGameBegin: (value: { start: boolean; category: string }) => void;
 };
 
-const Menu = ({ setIsMenuOpen, setGameBegin }: MenuTypes) => {
+const Menu = ({ title, setIsMenuOpen, setGameBegin }: MenuTypes) => {
   return (
     <div className="absolute inset-0 h-screen w-full z-[1] flex justify-center items-center select-none">
       <PageOverlay />
       <div className="w-[37rem] h-[27rem] gradient-bg inset--1 rounded-[72px] relative flex justify-center items-center">
         <header className="absolute top-[-20%] left-0 right-0 mx-auto z-[1]">
-          <SectionTitle text="Paused" />
+          <SectionTitle text={title} />
         </header>
         <MenuList setIsMenuOpen={setIsMenuOpen} setGameBegin={setGameBegin} />
       </div>
