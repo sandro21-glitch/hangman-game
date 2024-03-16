ea8abc13fb45c5d2528d21d14b09bd5431073962
Sandro <vipsandro11@gmail.com>
2024-03-16 17:34:32 +0400

    update SoundButton.tsx

--- src/features/welcomePage/SoundButton.tsx
+++ src/features/welcomePage/SoundButton.tsx
@@ -1,10 +1,38 @@
+import { useRef } from "react";
+
 import soundLogo from "/assets/sound.svg";
-// import soundOffLogo from "/assets/sound-off.svg";
+import soundOffLogo from "/assets/sound-off.svg";
+import musicSound from "/assets/sounds/ambiance.mp3";
+import { useAppDispatch, useAppSelector } from "../../hooks";
+import { toggleMusic } from "../game/hangmanSlice";
+
 const SoundButton = () => {
+  const dispatch = useAppDispatch();
+  const { music } = useAppSelector((store) => store.game);
+  const audioRef = useRef(new Audio(musicSound));
+  const handleStartMusic = () => {
+    dispatch(toggleMusic(!music));
+    if (!music) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+  };
+
   return (
-    <button className="absolute right-5 sm:right-[7rem] top-10 bg-primary-blue p-[.3rem] rounded-md inset--3">
-      <img src={soundLogo} alt="sound logo" />
-    </button>
+    <>
+      <button
+        type="button"
+        onClick={handleStartMusic}
+        className="absolute right-5 sm:right-[7rem] top-10 bg-primary-blue p-[.3rem] rounded-md inset--3"
+      >
+        <img src={music ? soundLogo : soundOffLogo} alt="sound logo" />
+      </button>
+      <audio ref={audioRef} loop>
+        <source src={musicSound} type="audio/mpeg" />
+        Your browser does not support the audio element.
+      </audio>
+    </>
   );
 };
 
