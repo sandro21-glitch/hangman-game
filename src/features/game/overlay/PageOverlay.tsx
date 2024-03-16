4612b3606ec1ba9b2fb1c9f3b8b476d8fbdaf28a
Sandro <vipsandro11@gmail.com>
2024-03-16 15:09:51 +0400

    update PageOverlay.tsx

--- src/features/game/overlay/PageOverlay.tsx
+++ src/features/game/overlay/PageOverlay.tsx
@@ -1,9 +1,8 @@
-
-import './PageOverlay.css'
+import "./PageOverlay.css";
 const PageOverlay = () => {
   return (
-    <div className="absolute h-screen w-full inset-0 bg-black opacity-[80%] z-[-2] overlayBg"></div>
-  )
-}
+    <div className="fixed h-screen w-full inset-0 bg-black opacity-[80%] z-[-2] overlayBg"></div>
+  );
+};
 
-export default PageOverlay
+export default PageOverlay;
