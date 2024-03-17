063cf28b31ed89bc89d7a272f88a2f53af88b69e
Sandro <vipsandro11@gmail.com>
2024-03-17 21:18:43 +0400

    feat:add lazy loading

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -3,13 +3,12 @@ import PlayBtn from "../features/welcomePage/PlayBtn";
 import SoundButton from "../features/welcomePage/SoundButton";
 import logo from "/assets/logo-desktop.svg";
 
-
-
 const WelcomePage = () => {
   return (
     <section className="flex-center min-h-screen relative">
       <article className="w-full sm:w-[37rem] h-[31rem] gradient-bg inset--1 flex relative rounded-[72px]">
         <img
+          loading="lazy"
           src={logo}
           alt="logo"
           className="w-[374px] h-auto absolute top-[-20%] left-0 right-0 mx-auto"
